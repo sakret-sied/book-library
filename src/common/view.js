a6271bac22ee1676b03eaa6026a3a306d711ff4f
SakretSied <sacretsid@gmail.com>
2023-11-22 16:20:16 +0400

    add basic routing

---
+++ src/common/view.js
@@ -0,0 +1,17 @@
+export class AbstractView {
+  constructor() {
+    this.app = document.querySelector("#root");
+  }
+
+  setTitle(title) {
+    document.title = title;
+  }
+
+  render() {
+    return;
+  }
+
+  destroy() {
+    return;
+  }
+}
